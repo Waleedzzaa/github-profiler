878c770f7c1e5ac68d2037aa18d4430128f9739d
Alwaleed Saleh Aljuhani <alwaleed11100@gmail.com>
2026-03-12 01:27:39 +0300

    Added contributions, next up is working on usernames, and username errors

--- index.js
+++ index.js
@@ -3,21 +3,20 @@ const chrome = require('selenium-webdriver/chrome');
 require('chromedriver');
 
 async function scrapeGitHubProfile(username) {
-    
     let driver = await new Builder().forBrowser(Browser.CHROME).build();
     try {
         await driver.get(`https://github.com/${username}`);
         
-        // Wait up to 5 seconds for the name element to appear in the DOM
         let nameElement = await driver.wait(until.elementLocated(By.css('.p-nickname')), 5000);
         
+        // Added the missing period (.) to correctly target the CSS class
+        let contrElement = await driver.wait(until.elementLocated(By.css('#js-contribution-activity-description')), 5000);
         
-        // Extract the visible text from the elements
         let fullName = await nameElement.getText();
-        
+        let contrib = await contrElement.getText();
         
         console.log(`Extracted Name: ${fullName}`);
-       ;
+        console.log(`Contributions: ${contrib}`);
     } catch (error) {
         console.error("Error:", error);
     } finally {
@@ -25,4 +24,4 @@ async function scrapeGitHubProfile(username) {
     }
 }
 
-scrapeGitHubProfile('flxkers');
+scrapeGitHubProfile('Mark');
